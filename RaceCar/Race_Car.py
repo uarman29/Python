737c4bf909245e9858836a56be4dd5576399b6cf
Uddin <auddin@dep.nyc.gov>
2019-08-30 15:26:17 -0400

    Adding Text

--- RaceCar/Race_Car.py
+++ RaceCar/Race_Car.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import *
+import time
 
 pygame.init()
 
@@ -12,60 +12,74 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 clock = pygame.time.Clock()
-crashed = False
 carImg = pygame.image.load("car.png")
 
 car_width = 100
 car_height = 125
 
+
 def placeCar(x,y):
     gameDisplay.blit(carImg,(x,y))
 
-x = (display_width*.45)
-y = (display_height*.45)
-x_change = 0
-y_change = 0
+def messageDisplay(text):
+    largeStyle = pygame.font.Font("freesansbold.ttf", 115)
+    text_surface = largeStyle.render(text, True, BLACK)
+    text_rect = text_surface.get_rect()
+    text_rect.center = (display_width/2, display_height/2)
+    gameDisplay.blit(text_surface, text_rect)
 
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+def gameLoop():
+    crashed = False
+    x = (display_width*.45)
+    y = (display_height*.45)
+    x_change = 0
+    y_change = 0
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-            elif event.key == pygame.K_UP:
-                y_change = -5
-            elif event.key == pygame.K_DOWN:
-                y_change = 5
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_change = 0
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+                elif event.key == pygame.K_UP:
+                    y_change = -5
+                elif event.key == pygame.K_DOWN:
+                    y_change = 5
+
+            if event.type == pygame.KEYUP:
+                if (x_change == -5 and event.key == pygame.K_LEFT) or (x_change == 5 and event.key == pygame.K_RIGHT):
+                    x_change = 0
+                if (y_change == -5 and event.key == pygame.K_UP) or (y_change == 5 and event.key == pygame.K_DOWN):
+                    y_change = 0
 
 
-    x += x_change
-    y += y_change
+        x += x_change
+        y += y_change
 
-    if x < -20:
-        x = -20
-    elif x > display_width - car_width:
-        x = display_width - car_width
+        if x < -20:
+            x = -20
+        elif x > display_width - car_width:
+            x = display_width - car_width
 
-    if y < 0:
-        y = 0
-    elif y > display_height - car_height:
-        y = display_height - car_height
+        if y < 0:
+            y = 0
+        elif y > display_height - car_height:
+            y = display_height - car_height
 
-    gameDisplay.fill(WHITE)
-    placeCar(x, y)
+        gameDisplay.fill(WHITE)
+        placeCar(x, y)
 
-    pygame.display.update()
-    clock.tick(60)
+        if x <= -20 or x >= display_width - car_width or y <= 0 or y >= display_height - car_height:
+            messageDisplay("GO BACK")
 
-pygame.quit()
-quit()
+        pygame.display.update()
+        clock.tick(60)
+
+    pygame.quit()
+    quit()
+
+gameLoop()
